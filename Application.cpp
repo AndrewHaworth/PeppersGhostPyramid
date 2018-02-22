573207f1fbe0723c68b9509409a2d0e5c7f04a0b
Andrew Haworth <the_andy_haworth@hotmail.com>
2018-02-22 14:32:47 +0000

    Added a header file. Commented out experimental Window options.

--- Application.cpp
+++ Application.cpp
@@ -1,11 +1,17 @@
 #include<opencv2\opencv.hpp>
+#include"application.h"
 
-using namespace cv;
 
 int main() {
+	menu();
+	return(0);
+}
+
+void camShow() {
+	using namespace cv;
 	VideoCapture cap;
 	if (!cap.open(0))
-		return 0;
+		return;
 	for (;;) {
 		Mat orig, frame, frame90, frame180, frame270;
 		cap >> orig;
@@ -19,16 +25,25 @@ int main() {
 
 		Mat3b res(3 * frame.rows, 3 * frame.cols, Vec3b(0, 0, 0));
 		frame.copyTo(res(Rect(frame.cols, 0, frame.cols, frame.rows)));
-		frame90.copyTo(res(Rect(frame90.rows*2, frame90.cols, frame90.cols, frame90.rows)));
-		frame180.copyTo(res(Rect(frame180.cols, frame180.rows*2, frame180.cols, frame180.rows)));
+		frame90.copyTo(res(Rect(frame90.rows * 2, frame90.cols, frame90.cols, frame90.rows)));
+		frame180.copyTo(res(Rect(frame180.cols, frame180.rows * 2, frame180.cols, frame180.rows)));
 		frame270.copyTo(res(Rect(0, frame270.cols, frame270.cols, frame270.rows)));
 		//namedWindow("Hypercam", WINDOW_NORMAL);
-		namedWindow("Hypercam", CV_WINDOW_KEEPRATIO);
+		namedWindow("Hypercam", WINDOW_GUI_EXPANDED);
 		//setWindowProperty("Hypercam", WND_PROP_FULLSCREEN, WINDOW_FULLSCREEN);
 
 		imshow("Hypercam", res);
 		if (waitKey(10) == 27)
 			break;
 	}
-	return(0);
 }
+
+void menu() {
+	using namespace std;
+	int i;
+	cout << "Hello and welcome to my project!" << endl;
+	cout << "What would you like to do today?" << endl;
+	cin >> i;
+	camShow();
+}
+
